c31fe4c033f6374f982f5fa2374c1dd00028654b
Palechonet <jeanbernardpierrelouis@gmail.com>
2026-04-19 07:15:38 +0200

    fix: remove invalid .catch on startScreenShare

--- components/LiveRoom.tsx
+++ components/LiveRoom.tsx
@@ -99,7 +99,7 @@ function Controls({ isHost, onLeave, onEnd, joined }: { isHost: boolean; onLeave
 
   const toggleScreen = useCallback(() => {
     if (isSharingScreen) stopScreenShare();
-    else startScreenShare().catch(console.error);
+    else { try { startScreenShare(); } catch (e) { console.error(e); } }
   }, [isSharingScreen, startScreenShare, stopScreenShare]);
 
   return (
