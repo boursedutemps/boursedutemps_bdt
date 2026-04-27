6a09e5d67826dfea03f4f538ae32067d6f0cd970
boursedutemps <boursedutemps@gmail.com>
2026-04-27 08:54:34 +0300

    fix: corriger chemins imports Forum.tsx

--- components/Forum.tsx
+++ components/Forum.tsx
@@ -3,8 +3,8 @@
 import React, { useState, useRef } from 'react';
 import Image from 'next/image';
 import { User, ForumTopic, MediaItem } from '../../types';
-import RichTextEditor from '../RichTextEditor';
-import { uploadToCloudinary } from '../../lib/useCloudinaryUpload';
+import RichTextEditor from './RichTextEditor';
+import { uploadToCloudinary } from '../lib/useCloudinaryUpload';
 import { Edit2, Trash2, MessageCircle, Heart, Share2 } from 'lucide-react';
 
 interface ForumProps {
