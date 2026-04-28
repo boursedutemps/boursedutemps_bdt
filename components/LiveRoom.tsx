ddb9f84834241f0244d1445bd5fe41426f4c7b6d
boursedutemps <boursedutemps@gmail.com>
2026-04-28 21:55:03 +0300

    fix: import livekit.css depuis app/

--- components/LiveRoom.tsx
+++ components/LiveRoom.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect, useRef, memo } from 'react';
 import { LiveKitRoom, VideoConference } from '@livekit/components-react';
-import '../../node_modules/@livekit/components-styles/dist/general/index.css';
+import '../app/livekit.css';
 import { supabase } from '../lib/supabaseClient';
 import { PhoneOff, X, PenTool } from 'lucide-react';
 
@@ -141,3 +141,4 @@ function LiveRoomComponent({
 }
 
 export default memo(LiveRoomComponent);
+
