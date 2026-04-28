cc0fcbfeccc36e68bcf20e657da00d6caa0b6093
boursedutemps <boursedutemps@gmail.com>
2026-04-28 22:29:49 +0300

    fix: transpilePackages livekit, serverExternalPackages dans experimental

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  serverExternalPackages: ['pg', 'bcryptjs', 'jsonwebtoken'],
+  experimental: {
+    serverExternalPackages: ['pg', 'bcryptjs', 'jsonwebtoken'],
+  },
+  transpilePackages: ['@livekit/components-react', '@livekit/components-core'],
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.postimg.cc',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
+      { protocol: 'https', hostname: 'i.postimg.cc' },
+      { protocol: 'https', hostname: 'picsum.photos' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
     ],
   },
 };
-
 export default nextConfig;
